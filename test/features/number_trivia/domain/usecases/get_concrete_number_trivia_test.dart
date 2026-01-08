ce21beaa3384bc1549a22223fe1544a7cff14076
Emdad Hossain <emdadhossain25@users.noreply.github.com>
2026-01-08 14:38:50 +0600

    live template added for dart test files

--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,17 +1,31 @@
+import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_architecture_tdd_course/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:clean_architecture_tdd_course/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 class MockNumberTriviaRepository extends Mock
-    implements NumberTriviaRepository{}
+    implements NumberTriviaRepository {}
 
-void main(){
+void main() {
   GetConcreteNumberTrivia usecase;
   MockNumberTriviaRepository mockNumberTriviaRepository;
 
-  setUp((){
+  setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
     usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
   });
+
+  final tNumber = 1;
+  final tQuoute = "test";
+  final tNumberTrivia = NumberTrivia(quote: tQuoute, id: tNumber);
+   test(
+         'should ',
+         () async {
+       // arrange
+
+       // act
+
+       // assert
+     });
 }
