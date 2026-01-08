61ba51e3083f2d721979f6fdedac4bd041443dd1
Emdad Hossain <emdadhossain25@users.noreply.github.com>
2026-01-08 17:50:16 +0600

    reading toJson also working fine

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:developer';
 
 import 'package:clean_architecture_tdd_course/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
